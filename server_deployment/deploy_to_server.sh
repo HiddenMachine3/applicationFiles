cb984c6798b9879d3c1db33c013571d87a7fdb5c
Test User <>
2022-11-02 02:48:56 +0530

    update

--- server_deployment/deploy_to_server.sh
+++ server_deployment/deploy_to_server.sh
@@ -1,18 +1,26 @@
 cd ..
-
 pwd
-#Generating digest.txt file
+
+#------
 export folder="$USERPROFILE\important jars"
-#folder="/opt/jar\ files"
+#export folder="/opt/jar\ files"
 #echo $folder
 
+#------appversion
 appver=$(date +%Y.%m.%d).$(date +"%T")
 appver=${appver//[:]/}
 appver=${appver//[.]/}
 echo this is the appversion : $appver
 
-#mkdir "app/$appver"
-
-#java -classpath "$folder\getdown-core-1.8.7.jar" com.threerings.getdown.tools.Digester "app/$appver"
+#------generating digest
 java -classpath "$folder\getdown-core-1.8.7.jar" com.threerings.getdown.tools.Digester "app/beta"
+
+#------copying contents to new version's directory
+mkdir "app/$appver"
+cp -r app/beta/* "app/$appver"
+
+#------adding folder to git
+git add app/$appver/*
+git commit -m "added app version $appver"
+git push
 #read varname
