2697066cec9a6f6d54f8bf2d99eaca61d96a7424
Test User <>
2022-11-02 02:05:20 +0530

    update

--- server_deployment/deploy_to_server.sh
+++ server_deployment/deploy_to_server.sh
@@ -11,7 +11,8 @@ appver=${appver//[:]/}
 appver=${appver//[.]/}
 echo this is the appversion : $appver
 
-mkdir "app/$appver"
+#mkdir "app/$appver"
 
-java -classpath "$folder\getdown-core-1.8.7.jar" com.threerings.getdown.tools.Digester "app/$appver"
+#java -classpath "$folder\getdown-core-1.8.7.jar" com.threerings.getdown.tools.Digester "app/$appver"
+java -classpath "$folder\getdown-core-1.8.7.jar" com.threerings.getdown.tools.Digester "app/beta"
 read varname
